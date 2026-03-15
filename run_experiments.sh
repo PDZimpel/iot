e1d595ee1bf53fd3e370d3b2eda70dde4e5f4ac1
Paulo Zimpel <pzimpel15@gmail.com>
2026-03-15 17:16:12 -0400

    feat: tests include different thread values

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,13 +1,22 @@
-inputs=("1_000nds" "10_000nds" "100_000nds")
+inputs=("1_000nds" "10_000nds")
 ccns=(100 1000 10000 100000)
+tsizes=(2 4 8)
 
 for input in "${inputs[@]}"; do
-	mkdir out/p/$input
-	mkdir out/s/$input
+	mkdir -p out/p/$input
+	mkdir -p out/s/$input
+	for tsize in "${tsizes[@]}";do
+		export OMP_NUM_THREADS="$tsize"
+		echo "Executing with num threads = $OMP_NUM_THREADS"
+		mkdir -p out/p/$input/$OMP_NUM_THREADS	
+		for ccn in "${ccns[@]}"; do
+			echo "ccn: $ccn; input $input"
+			echo "Parallel Run, $OMP_NUM_THREADS threads"
+			./build/openmp_fixed_dimna/openmp_fixed_di_mna --in ../MNA/inputs/parquet/$input --out out/p/$input/$OMP_NUM_THREADS --runs 1 --ccn $ccn --cs 2
+		done
+	done
 	for ccn in "${ccns[@]}"; do
 		echo "ccn: $ccn; input $input"
-		echo "Parallel Run"
-		./build/openmp_fixed_dimna/openmp_fixed_di_mna --in ../MNA/inputs/parquet/$input --out out/p/$input/ --runs 1 --ccn $ccn --cs 2
 		echo "Serial Run"
 		./build/dimna/di_mna --in ../MNA/inputs/parquet/"$input"/ --out out/s/$input/ --runs 1 --ccn $ccn --cs 2
 	done
